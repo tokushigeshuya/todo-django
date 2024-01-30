4ea732052a66794be39e09af576a4ad598f0adf5
tokushigeshuya <narihare1130@gmail.com>
2024-01-30 16:58:15 +0900

    [add]詳細ページの構築

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from todo.views import TaskListVies
+from todo.views import TaskListVies,TaskDetailView
 
 urlpatterns = [
-    path('', TaskListVies.as_view(), name='task-list')
+    path('', TaskListVies.as_view(), name='task-list'),
+    path('task/<int:pk>/', TaskDetailView.as_view(), name='task-deatail'),
 ]
 
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView,CreateView
+from django.urls import reverse_lazy
 
 from todo.models import Task
 # Create your views here.
 class TaskListVies(ListView):
   model = Task
   template_name = 'todo/task_list.html'
+
+class TaskDetailView(DetailView):
+  model = Task
+  template_name = 'todo/task_detail.html'
+
+class TaskCreateView(CreateView):
+  model = Task
+  fields = '__all__'
+  success_url = reverse_lazy('task-list')
+  template_name = 'todo/task_detail.html'
