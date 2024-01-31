ccfe27d33cd8a765785004bc56765c13e36b5e2f
tokushigeshuya <narihare1130@gmail.com>
2024-02-01 00:07:58 +0900

    [add]タスク削除の機能実装

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from todo.views import TaskListVies,TaskDetailView,TaskCreateView
+from todo.views import TaskListVies,TaskDetailView,TaskCreateView,TaskDeleteView
 
 urlpatterns = [
     path('', TaskListVies.as_view(), name='task-list'),
     path('task/new/', TaskCreateView.as_view(), name='task-new'),
     path('task/<int:pk>/', TaskDetailView.as_view(), name='task-deatail'),
+    path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
 ]
 
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView,CreateView
+from django.views.generic import ListView,DetailView,CreateView,DeleteView
 from django.urls import reverse_lazy
 
 from todo.models import Task
@@ -17,3 +17,8 @@ class TaskCreateView(CreateView):
   fields = '__all__'
   success_url = reverse_lazy('task-list')
   template_name = 'todo/task_form.html'
+
+class TaskDeleteView(DeleteView):
+  model = Task
+  success_url = reverse_lazy('task-list')
+  template_name = 'todo/task_delete.html'
