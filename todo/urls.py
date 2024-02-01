7583eca214aaeab7d7e449f7ea7c2a82e00074dc
tokushigeshuya <narihare1130@gmail.com>
2024-02-01 23:11:22 +0900

    [add]CRUD機能の実装

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from todo.views import TaskListVies,TaskDetailView,TaskCreateView,TaskDeleteView
+from todo.views import (
+  TaskListVies,
+  TaskDetailView,
+  TaskCreateView,
+  TaskDeleteView,
+  TaskUpdateView
+  )
 
 urlpatterns = [
     path('', TaskListVies.as_view(), name='task-list'),
     path('task/new/', TaskCreateView.as_view(), name='task-new'),
     path('task/<int:pk>/', TaskDetailView.as_view(), name='task-deatail'),
     path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
+    path('task/<int:pk>/edit/', TaskUpdateView.as_view(), name='task-edit'),
 ]
 
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView,CreateView,DeleteView
+from django.views.generic import ListView,DetailView,CreateView,DeleteView,UpdateView
 from django.urls import reverse_lazy
 
 from todo.models import Task
@@ -22,3 +22,10 @@ class TaskDeleteView(DeleteView):
   model = Task
   success_url = reverse_lazy('task-list')
   template_name = 'todo/task_delete.html'
+
+class TaskUpdateView(UpdateView):
+  model = Task
+  fields = '__all__'
+  success_url = reverse_lazy('task-list')
+
+  
