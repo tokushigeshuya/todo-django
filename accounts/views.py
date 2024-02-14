a1a1e902260d9f5c736b81665eb0f04a0538a9e8
tokushigeshuya <narihare1130@gmail.com>
2024-02-15 00:14:27 +0900

    [add]ログイン機能の実装

---
+++ accounts/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.views import LoginView
+
+# Create your views here.
+
+class MyLoginView(LoginView):
+  template_name = 'accounts/login.html',
+  # falseだとログイン後もログイン画面に戻ってくる trueで他のURL設定が可能
+  # false → login/ → login/
+  # true → login/ → 　task-list/
+  redirect_autheticated_user = True,
--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('todo.urls'))
+    path('', include('todo.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
